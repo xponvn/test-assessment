4fbdbb38380d89a80721d76dff9aa9107db554a8
fallingforest <lam.vu@xpon.ai>
2023-03-25 20:26:52 +0700

    init storybook, design tokens & example ui component

--- apps/frontend-web/tailwind.config.js
+++ apps/frontend-web/tailwind.config.js
@@ -1,12 +1,3 @@
 /** @type {import('tailwindcss').Config} */
 
-import { tokens } from '@test-assessment/design-token';
-
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-    colors: tokens.colors
-  },
-  plugins: []
-};
+module.exports = require('@test-assessment/ui-theme');
---
+++ packages/ui-theme/src/index.ts
@@ -0,0 +1,10 @@
+import { tokens } from '@test-assessment/design-token';
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    colors: tokens.colors,
+  },
+  plugins: [],
+};
