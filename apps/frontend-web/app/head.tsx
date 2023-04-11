928b6869aef6321e3462cd7dd5cb876fb1ebeac5
vunam2000 <vunam722000@gmail.com>
2023-04-11 14:17:38 +0700

    Test action

--- apps/frontend-web/app/head.tsx
+++ apps/frontend-web/app/head.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function RootHead() {
-  return (
-    <title>Test Assessment</title>
-  )
+  return <title>Test Assessment !!</title>;
 }
