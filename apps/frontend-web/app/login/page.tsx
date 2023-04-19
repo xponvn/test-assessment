3f34582cdd27a832431f4f41c8027e03173a2c9c
trungdt705 <dinhthanhtrung7051992@gmail.com>
2023-04-19 14:46:52 +1000

    update ui

--- apps/frontend-web/app/login/page.tsx
+++ apps/frontend-web/app/login/page.tsx
@@ -1,76 +1,107 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 
-export default function page() {
-  const handleKeyboardEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    console.log(123);
+export default function Page() {
+  const [isEmailError, setIsEmailError] = useState<boolean>(false);
+  const [isPasswordError, setIsPasswordError] = useState<boolean>(false);
+
+  const regexEmail = new RegExp('^[A-Za-z0-9._%+-]+@xpon.ai$');
+  const regexPassword = new RegExp(
+    '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{7,}$'
+  );
+
+  const handleEmail = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!regexEmail.test('123')) {
+      setIsEmailError(true);
+    } else {
+      setIsEmailError(false);
+    }
   };
 
-  const test = () => {
-    console.log(124324);
+  const handlePassword = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!regexPassword.test('123')) {
+      setIsPasswordError(true);
+    } else {
+      setIsPasswordError(false);
+    }
   };
   return (
-    <div className="grid grid-flow-col auto-cols-max px-52">
-      <div>
-        <img src="/images/login_bg.jpg" alt="" className="h-auto max-w-full" />
+    <div className="flex flex-col md:flex-row h-screen items-center">
+      <div className="w-full md:w-1/2 xl:w-4/7 h-screen">
+        <img
+          className="object-cover w-full h-full"
+          src="/images/login_bg.jpg"
+          alt="img"
+        />
       </div>
-      <div className="p-24">
-        <img src="/images/xpon_logo.png" alt="" className="mb-32" />
-        <p className="text-40 font-bold text-neutral-text-secondary">Login</p>
-        <p className="text-15 text-neutral-text-secondary mb-10">
-          Please sign in with your provided account and password
-        </p>
-
-        <form>
-          <div className="grid mb-6 md:grid-cols-1">
-            <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-neutral-placeholder text-13 font-medium"
-              >
-                Email
-              </label>
-              <input
-                type="text"
-                id="first_name"
-                className="bg-gray-50 border border-neutral-border block w-full p-2.5 placeholder-neutral-border text-15 mb-2"
-                placeholder="Enter your email"
-                onKeyDown={handleKeyboardEvent}
-                required
-              />
-              <span className="text-13 text-error-base">123123</span>
-            </div>
+      <div className="w-full md:w-1/2 xl:w-4/7 h-screen">
+        <div className="flex p-24 w-3/7">
+          <div>
+            <img src="/images/xpon_logo.png" alt="" className="mb-32" />
+            <p className="text-40 font-bold text-neutral-text-secondary">
+              Login
+            </p>
+            <p className="text-15 font-normal text-neutral-text-secondary mb-10">
+              Please sign in with your provided account and password
+            </p>
+            <form>
+              <div className="grid mb-6 md:grid-cols-1">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-neutral-placeholder text-13 font-medium"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    id="first_name"
+                    className="bg-gray-50 border border-neutral-border block w-full p-2.5 placeholder-neutral-border text-15 mb-2"
+                    placeholder="Enter your email"
+                    onKeyDown={handleEmail}
+                    required
+                  />
+                  <span className="text-13 text-error-base">
+                    {isEmailError && 'Please enter an email address'}
+                  </span>
+                </div>
+              </div>
+              <div className="grid mb-6 md:grid-cols-1">
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-neutral-placeholder text-13 font-medium"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="text"
+                    id="first_name"
+                    className="bg-gray-50 border border-neutral-border block w-full p-2.5 placeholder-neutral-border text-15  mb-2"
+                    placeholder="Enter your password"
+                    onKeyDown={handlePassword}
+                    required
+                  />
+                  <span className="text-13 text-error-base">
+                    {isPasswordError && 'Incorrect password'}
+                  </span>
+                </div>
+              </div>
+              <div className="grid mb-6 md:grid-cols-1">
+                <button
+                  type="button"
+                  className="text-neutral-text-primary font-medium px-5 py-2.5 text-center bg-primary-base"
+                  onClick={() => {
+                    console.log(123);
+                  }}
+                >
+                  LOGIN
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="grid mb-6 md:grid-cols-1">
-            <div>
-              <label
-                htmlFor="password"
-                className="block mb-2 text-neutral-placeholder text-13 font-medium"
-              >
-                Password
-              </label>
-              <input
-                type="text"
-                id="first_name"
-                className="bg-gray-50 border border-neutral-border block w-full p-2.5 placeholder-neutral-border text-15  mb-2"
-                placeholder="Enter your password"
-                required
-              />
-              <span className="text-13 text-error-base">123123</span>
-            </div>
-          </div>
-          <div className="grid mb-6 md:grid-cols-1">
-            <button
-              type="button"
-              className="text-neutral-text-primary font-medium px-5 py-2.5 text-center bg-primary-base"
-              onClick={() => {
-                console.log(123);
-              }}
-            >
-              LOGIN
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
