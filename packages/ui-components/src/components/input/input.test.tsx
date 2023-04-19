f4c3681299c7ca5111f7d6d81b8bde93f2eaa670
Chung Nguyen <chungnguyen011298@gmail.com>
2023-04-19 17:51:12 +0700

    [XDLD-234] input component

---
+++ packages/ui-components/src/components/input/input.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Input } from './input';
+
+describe('Button', () => {
+  it(`should render input successfully`, () => {
+    const { baseElement } = render(
+      <Input type="text" value="this is value" width={300} />
+    );
+    expect(baseElement).toMatchSnapshot();
+  });
+});
