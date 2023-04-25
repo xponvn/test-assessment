0beada671781179f83e599fa6dd08409dc914d4c
HungPQinforma <hung.pham@xpondigital.com>
2023-04-25 23:30:13 +0700

    Refactor & commonize props interface of Input

--- packages/ui-components/src/components/input/input.tsx
+++ packages/ui-components/src/components/input/input.tsx
@@ -1,25 +1,50 @@
-import * as React from 'react';
-import { Icon } from '../icons';
+import React from 'react';
+import { InputError } from './inputError';
+import { InputIcon, InputIconType } from './inputIcon';
 
 export interface InputProps
-  extends React.DetailedHTMLProps<
-    React.InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    'size'
   > {
   label?: string;
   error?: string;
   width?: number;
   block?: boolean;
+  leftIcon?: InputIconType;
+  rightIcon?: InputIconType;
+  placeholder?: string;
+  disabled?: boolean;
+  size?: InputSize;
+  helperText?: string;
+  successText?: string;
+}
+export enum InputType {
+  PASSWORD = 'password',
+  TEXT = 'text',
+  DATETIME = 'datetime',
+}
+export enum InputSize {
+  LARGE = 'lg',
+  MEDIUM = 'md',
+  SMALL = 'sm',
 }
 
 export const Input = React.forwardRef(
   (
     {
+      name,
       label,
       error = '',
-      type = 'text',
+      type = InputType.TEXT,
       width = 200,
       block = false,
+      size = InputSize.MEDIUM,
+      rightIcon,
+      leftIcon,
       ...props
     }: InputProps,
     ref
@@ -28,37 +53,32 @@ export const Input = React.forwardRef(
 
     return (
       <div className="space-y-2">
-        {label && (
-          <label
-            htmlFor={props.name}
-            className="block text-13 leading-6 font-medium font-primary text-neutral-placeholder"
-          >
-            {label}
-          </label>
-        )}
+        {label && <Label htmlFor={name} label={label} />}
 
         <div
-          className={`relative text-15 leading-6 font-normal font-primary border-[1px] p-3 ${type === 'password' ? 'pr-12' : 'pr-3'
-            } ${block ? 'w-full' : 'w-fit'} ${error
+          className={`relative text-15 leading-6 font-normal font-primary border-[1px] p-3 ${
+            type === 'password' ? 'pr-12' : 'pr-3'
+          } ${block ? 'w-full' : 'w-fit'} ${
+            error
               ? 'border-error-border bg-error-bg'
               : 'border-neutral-border placeholder-neutral-border'
-            }`}
+          }`}
         >
+          {leftIcon && <InputIcon type={type} />}
+
           <input
             {...props}
             // @ts-expect-error passing input ref
             ref={ref}
-            id={props.name}
+            id={name}
             type={currentType}
-            onChange={(event) => {
-              if (props.onChange) props.onChange(event);
-            }}
             className={`focus:outline-none`}
             aria-invalid={Boolean(error)}
-            aria-describedby={error ? `${props.name}-error` : undefined}
+            aria-describedby={error ? `${name}-error` : undefined}
             style={{ ...props.style, width: block ? '100%' : width + 'px' }}
           />
-          {type === 'password' && (
+
+          {type === InputType.PASSWORD && (
             <button
               className="absolute right-3 top-3"
               onClick={() => {
@@ -67,39 +87,32 @@ export const Input = React.forwardRef(
                 );
               }}
             >
-              <Icon
-                name={currentType === 'password' ? 'eye-show' : 'eye-hide'}
-                className="text-neutral-placeholder !w-6 !h-6"
+              <InputIcon
+                type={
+                  currentType === 'password'
+                    ? InputIconType.PASSWORD_HIDDEN
+                    : InputIconType.PASSWORD_VISIBLE
+                }
               />
             </button>
           )}
+
+          {rightIcon && <InputIcon type={type} />}
         </div>
 
         <div className={error ? 'opacity-100' : 'opacity-0'}>
-          <InputError name={props.name}>{error}</InputError>
+          <InputError>{error}</InputError>
         </div>
       </div>
     );
   }
 );
 
-export const InputError = ({
-  name,
-  children,
-}: React.PropsWithChildren<{ name?: string }>) => {
-  if (!children) {
-    return null;
-  }
-
-  return (
-    <div className="flex space-x-1 items-center">
-      <Icon name="caution" className="text-error !w-6 !h-6" />
-      <p
-        id={name ? `${name}-error` : undefined}
-        className="text-error text-13 font-normal leading-6 font-primary"
-      >
-        {children}
-      </p>
-    </div>
-  );
-};
+const Label = ({ htmlFor, label }: { htmlFor?: string; label: string }) => (
+  <label
+    htmlFor={htmlFor}
+    className="block text-13 leading-6 font-medium font-primary text-neutral-placeholder"
+  >
+    {label}
+  </label>
+);
