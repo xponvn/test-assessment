f8cfe9022c37373f13321efea548ba1535c15379
nvt1904 <nvt1904@gmail.com>
2023-04-26 09:22:37 +0700

    [Security] Configure role for API

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -64,4 +64,16 @@ export default ({ env }) => ({
       },
     },
   },
+  'config-sync': {
+    enabled: true,
+    config: {
+      syncDir: 'config/sync/',
+      minify: false,
+      soft: true,
+      importOnBootstrap: false,
+      customTypes: [],
+      excludedTypes: [],
+      excludedConfig: ['core-store.plugin_users-permissions_grant'],
+    },
+  },
 });
