2382fab04cc20a28b84e56e59c0ca37f67e2f4c5
tungto <tung.to@informa.com>
2023-05-04 17:06:02 +0700

    rebase

---
+++ apps/cms/src/api/individual-test-page/routes/individual-test-page.ts
@@ -0,0 +1,7 @@
+/**
+ * individual-test-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::individual-test-page.individual-test-page');
---
+++ apps/cms/src/api/individual-test-page/services/individual-test-page.ts
@@ -0,0 +1,7 @@
+/**
+ * individual-test-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::individual-test-page.individual-test-page');
