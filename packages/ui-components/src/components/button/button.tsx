4dc1f36b1ee16b2ff6af9bd715d9c361f398854e
phongnttt <111858261+phongnttt@users.noreply.github.com>
2023-05-04 14:57:23 +0700

    add variant and size

--- packages/ui-components/src/components/button/button.tsx
+++ packages/ui-components/src/components/button/button.tsx
@@ -7,6 +7,8 @@ interface BaseButtonProps {
   RightIcon?: React.ReactNode;
   className?: string;
   block?: boolean;
+  variant?: 'primary' | 'secondary' | 'display' | 'secondaryDark';
+  size?: 'small' | 'medium' | 'large';
 }
 
 interface NormalButtonProps extends BaseButtonProps {
@@ -26,12 +28,33 @@ export type ButtonProps = NormalButtonProps | LinkButtonProps;
 // TODO: apply design
 const buttonClassName: Record<BaseButtonProps['type'], string> = {
   button:
-    'items-center justify-center px-4 py-2 bg-primary-base font-medium font-primary text-13 leading-6 space-x-2',
+    'items-center justify-center px-4 py-2 font-medium font-primary text-13 leading-6 space-x-2',
   link: 'inline-flex items-center justify-center space-x-2 text-primary-base font-normal font-primary text-15 leading-6',
 };
 
+const variantClassName: Record<string, string> = {
+  display: 'bg-primary-base shadow-[5px_5px_0] shadow-secondary-base',
+  primary: 'bg-primary-base',
+  secondary:
+    'border-[3px] border-solid border-primary-base bg-primary-background',
+  secondaryDark: 'border-[3px] border-solid border-primary-base',
+};
+const sizeClassName: Record<string, string> = {
+  small: 'px-2 py-1',
+  medium: 'px-4 py-2',
+  large: 'px-6 py-3',
+};
+
 export const Button = React.forwardRef((props: ButtonProps, ref) => {
-  const { children, type, className = '', LeftIcon, RightIcon } = props;
+  const {
+    children,
+    type,
+    className = '',
+    LeftIcon,
+    RightIcon,
+    variant = 'primary',
+    size = 'medium',
+  } = props;
 
   if (type === 'link') {
     return (
@@ -56,7 +79,7 @@ export const Button = React.forwardRef((props: ButtonProps, ref) => {
       ref={ref}
       className={`${buttonClassName[type]} ${className} ${
         props.block ? 'w-full flex' : 'inline-flex'
-      }`}
+      } ${variantClassName[variant]} ${sizeClassName[size]}`}
       onClick={props.onClick}
     >
       {Boolean(LeftIcon) && LeftIcon}
